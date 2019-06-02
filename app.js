4e568857681533fb1894c8b1da5a85d91ff8093b
Sinder <lixingda2012@foxmail.com>
2019-06-02 14:51:15 +0800

    什么是async和await

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const koa = require("koa");
 
 const app = new koa();
-// 在浏览器访问localhost:3000 打印出结果是1 3 4 2
-// 洋葱模型
+// 加上async关键字，会把当前的函数改为promise的形式
 app.use(async (ctx, next) => {
-  console.log(1);
-  // 根据中间件的上下文顺序依次调用
-  await next();
-  console.log(2);
-});
-
-app.use(async (ctx, next) => {
-    // promise 的异步编程 async和await是终极解决方案
-  console.log(3);
-  await next();
-  console.log(4);
+  const axios = require("axios");
+  // axios返回的是promise
+  // 1、await会求值，自动帮忙算出promise中正确返回的数据
+  // 2、await会阻塞线程，把异步的执行方式改为同步的，如果await后面的数据没有返回，程序会一直等待
+  const start = Date.now();
+  const res = await axios.get("https://baidu.com");
+  const end = Date.now();
+  console.log(res);
+  console.log("时间差:", end - start);
 });
 
 app.listen(3000);
