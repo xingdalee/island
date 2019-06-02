3682dd31fce2c9badfd913389d554fe53053684e
Sinder <lixingda2012@foxmail.com>
2019-06-02 12:37:58 +0800

    洋葱模型和async await的使用

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const koa = require("koa");
 
 const app = new koa();
-
-app.use((ctx, next) => {
-  console.log("第1个中间件");
+// 在浏览器访问localhost:3000 打印出结果是1 3 4 2
+// 洋葱模型
+app.use(async (ctx, next) => {
+  console.log(1);
   // 根据中间件的上下文顺序依次调用
-  next();
+  await next();
+  console.log(2);
 });
 
-app.use((ctx, next) => {
-  console.log("第2个中间件");
+app.use(async (ctx, next) => {
+    // promise 的异步编程 async和await是终极解决方案
+  console.log(3);
+  await next();
+  console.log(4);
 });
 
 app.listen(3000);
