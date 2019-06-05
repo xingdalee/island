565e0546540a98c485b3ed18e49977b706fafa3a
Sinder <lixingda2012@foxmail.com>
2019-06-05 23:05:55 +0800

    使用require-directory自动加载和koa-router

--- api/v1/book.js
+++ api/v1/book.js
@@ -1,6 +1,5 @@
 const Router = require("koa-router");
 const router = new Router();
-console.log(1111111111)
 
 router.get("/v1/book/latest", (ctx, next) => {
   ctx.body = { key: "v1book" };
--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const Router = require("koa-router");
 const requireDirectory = require("require-directory");
 // 借助requireDirectory可以自动加载api下所有的js文件
 requireDirectory(module, "./api", {
-  visit: WenLoadModules
+  visit: obj => {
+    app.use(obj.routes());
+  }
 });
-function WenLoadModules(obj) {
-  app.use(obj.routes());
-}
+
 app.listen(3000);
