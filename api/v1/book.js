7f5453fa4eb384811366520f578948c235d291c1
Sinder <lixingda2012@foxmail.com>
2019-06-05 23:03:33 +0800

    修复路径问题

--- api/v1/book.js
+++ api/v1/book.js
@@ -2,7 +2,7 @@ const Router = require("koa-router");
 const router = new Router();
 console.log(1111111111)
 
-router.get("v1/book/latest", (ctx, next) => {
+router.get("/v1/book/latest", (ctx, next) => {
   ctx.body = { key: "v1book" };
 });
 module.exports = router;
--- api/v2/book2.js
+++ api/v2/book2.js
@@ -1,6 +1,6 @@
 const Router = require("koa-router");
 const router = new Router();
-router.get("v2/book/latest", (ctx, next) => {
+router.get("/v2/book/latest", (ctx, next) => {
   ctx.body = { key: "v2 book" };
 });
 module.exports = router;
