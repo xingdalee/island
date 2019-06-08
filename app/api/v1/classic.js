a4f66edd67f2434cbe329a82a93479cd4a381441
Sinder <lixingda2012@foxmail.com>
2019-06-08 16:11:32 +0800

    根据不同的错误类型，增加不同的类

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,9 +1,9 @@
 const Router = require("koa-router");
 const router = new Router();
-const { HttpException } = require("../../../core//HttpException");
+const { ParameterException } = require("../../../core//HttpException");
 router.post("/v1/classic/latest", (ctx, next) => {
   const body = ctx.request.body;
   ctx.body = { key: "v1book" };
-  throw new HttpException("错误啦", 10001, 400);
+  throw new ParameterException("哎呀，错误啦");
 });
 module.exports = router;
