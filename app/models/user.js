3abc07164e2f5c91a496e26451dfcedd7a4baa88
Sinder <lixingda2012@foxmail.com>
2019-06-11 20:58:39 +0800

    使用model的set方法获取并加密password

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,6 @@
 const { sequelize } = require("../../core/db");
 const { Sequelize, Model } = require("sequelize");
-
+const bcryptjs = require("bcryptjs");
 class User extends Model {}
 User.init(
   {
@@ -15,7 +15,16 @@ User.init(
       type: Sequelize.STRING(128),
       unique: true // 是否唯一
     },
-    password: Sequelize.STRING,
+    password: {
+      type: Sequelize.STRING,
+      set(val) {
+        // 数值越大，加密越复杂，消耗的性能也越多
+        const salt = bcryptjs.genSaltSync(10);
+        const pwd = bcryptjs.hashSync(val, salt);
+        // model里的属性操作，这样可以把数据保存到数据库
+        this.setDataValue("password", pwd);
+      }
+    },
     openid: {
       type: Sequelize.STRING(64),
       unique: true // 是否唯一
