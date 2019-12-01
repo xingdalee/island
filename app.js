1d890933a3a3691b9d5aaba00a6a881990e9efd0
Sinder <1028263368@qq.com>
2019-12-01 17:19:18 +0800

    第一个get请求

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 /*
  * @Author: lixingda
- * @Desc: 洋葱模型示例
- * @Date: 2019-11-29 10:41:29
+ * @Desc: 第一个get请求
+ * @Date: 2019-12-01 16:58:52
  * @Last Modified by: lixingda
- * @Last Modified time: 2019-11-29 13:24:11
+ * @Last Modified time: 2019-12-01 17:18:29
  */
+
 const koa = require("koa");
 
 const app = new koa();
 
-app.use(() => {
-  console.log("你好，蒲惠。");
+app.use((ctx, next) => {
+  if (ctx.path === "/getMessage" && ctx.method === "GET") {
+    ctx.body = {
+      message: "成功获取了请求"
+    };
+  }
 });
-
-// 如何调用下一个中间件？
-// app.use(() => {
-//   console.log("你好，Koa。");
-// });
 app.listen(3000);
