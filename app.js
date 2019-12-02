6a6a87d7548fd803a7c2f5735e47c4687a467443
Sinder <lixingda2012@foxmail.com>
2019-12-02 14:25:23 +0800

    接口获取http参数的方法

--- app.js
+++ app.js
@@ -3,18 +3,39 @@
  * @Desc: 第一个get请求
  * @Date: 2019-12-01 16:58:52
  * @Last Modified by: lixingda
- * @Last Modified time: 2019-12-01 17:18:29
+ * @Last Modified time: 2019-12-02 14:24:56
  */
 
 const koa = require("koa");
+const bodyParser = require("koa-bodyparser");
 
 const app = new koa();
+app.use(bodyParser());
+
+app.use(async (ctx, next) => {
+  if (ctx.path === "/getMessage" && ctx.method === "GET") {
+    const { query, body } = ctx;
+    ctx.body = {
+      message: "成功获取了请求",
+      query,
+      body
+    };
+  }
+  await next();
+});
 
 app.use((ctx, next) => {
-  if (ctx.path === "/getMessage" && ctx.method === "GET") {
+  if (ctx.path === "/insertMessage" && ctx.method === "POST") {
+    const {
+      query,
+      request: { body }
+    } = ctx;
     ctx.body = {
-      message: "成功获取了请求"
+      message: "插入数据成功",
+      query,
+      body
     };
   }
 });
+
 app.listen(3000);
